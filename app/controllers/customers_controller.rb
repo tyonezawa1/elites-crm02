5ff113d48f5e2145de6f71e62184a9b91ec55c9b
Yone <yonezawa-takahiro@av.avex.co.jp>
2016-11-08 10:40:25 +0000

    first commit

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,62 @@
+class CustomersController < ApplicationController
+  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  
+  def index
+    @q = Customer.search(params[:q])
+    @customers = @q.result(distinct: true).page(params[:page])
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+    @comment = Comment.new
+    @comments = @customer.comments
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to root_path
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email,
+      :company_id,
+      :post_id
+      )
+  end
+  
+end
