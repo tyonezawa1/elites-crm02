344326f14ebd70b2e51836bcefd2233b4be350f7
Yone <yonezawa-takahiro@av.avex.co.jp>
2016-11-14 09:23:56 +0000

    <08_基本課題完了>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -30,11 +30,17 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
+    
     if @comment.save
       redirect_to customer_path(@comment.customer_id)
     else
-      redirect_to customer_path(@comment.customer_id)
+      @customer = Customer.find(@comment.customer_id)
+      @comments = @customer.comments
+       render template:"customers/show"
     end
+    
+  
+    
   end
 
   private
