7e89ac28dd514581f8b35c6ea337d2b8d91e4938
Yone <yonezawa-takahiro@av.avex.co.jp>
2016-11-15 13:14:40 +0000

    <基本課題９と１０完了しました>

---
+++ db/migrate/20161115115000_add_colum_user_id.rb
@@ -0,0 +1,5 @@
+class AddColumUserId < ActiveRecord::Migration
+  def change
+    add_column:customers, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106134203) do
+ActiveRecord::Schema.define(version: 20161115115000) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "customer_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["customer_id"], name: "index_comments_on_customer_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161106134203) do
     t.datetime "updated_at",  null: false
     t.integer  "company_id"
     t.integer  "post_id"
+    t.string   "user_id"
   end
 
   add_index "customers", ["company_id"], name: "index_customers_on_company_id"
@@ -65,6 +68,7 @@ ActiveRecord::Schema.define(version: 20161106134203) do
     t.datetime "updated_at",                          null: false
     t.string   "family_name"
     t.string   "given_name"
+    t.string   "image_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
